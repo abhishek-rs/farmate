e725528398f80547c89f1385ebc2b25008bdb6f6
shek <abhishek10.rs@gmail.com>
2017-07-14 19:23:31 +0300

    fixed the weather widget once and for all

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,20 +4,6 @@ import ReactAnimatedWeather from 'react-animated-weather'
 import '../styles/Weather.css'
 import ReactDOM from 'react-dom'
 
-const iconBridge = {
-	'clear sky': 'CLEAR_DAY',
-	'clear-night': 'CLEAR_NIGHT',
-	'few clouds': 'PARTLY_CLOUDY_DAY',
-	'scattered clouds': 'PARTLY_CLOUDY_NIGHT',
-	'broken clouds': 'CLOUDY',
-	'shower rain': 'RAIN',
-	'rain': 'SLEET',
-	'thunderstorm': 'RAIN',
-	'snow': 'SNOW',
-	'wind': 'WIND',
-	'mist': 'FOG'
-};
-
 const defaults = {
   		icon: 'CLEAR_DAY',
   		color: 'white',
@@ -51,9 +37,9 @@ getPosition(position){
 }
 
 getCurrentWeather(){
-	let baseUrl = 'http://api.openweathermap.org/data/2.5/weather?appid=cbbea6373c90b88fa24a0e125e8e765b';
+	let baseUrl = 'https://api.apixu.com/v1/current.json?key=5cc4c4116ec54fb98a0153211171407&q=';
 	var that = this;
-	Request.get(baseUrl + "&lat=" + this.state.latitude + "&lon=" + this.state.longitude)
+	Request.get(baseUrl + this.state.latitude + "," + this.state.longitude)
 			.end(function(err, res){
 				if(err) 
 					console.log(err);
@@ -74,25 +60,30 @@ let weather = this.state.currentWeather;
   return (
     <div> { this.state.dataReceived  ? 
 			
-	<div id = "weatherWidget" >
-		<div id="summaryWeather">
-		<h2> {weather.weather[0].description.charAt(0).toUpperCase() + weather.weather[0].description.slice(1)} </h2>
-		<h4> {weather.name}, {weather.sys.country} </h4>
-		</div>
-		<div id="icon">
-			<ReactAnimatedWeather icon={iconBridge[this.state.currentWeather.weather[0].description]} color={defaults.color} size={defaults.size} animate={defaults.animate} />
-		</div>
-		<div id = "temperature"> 
-			<h4> Temperature { Math.round((weather.main.temp - 273) * 100) / 100} °C</h4>
-		</div>
-		<div id="humidity">
-			<h4> Humidity {weather.main.humidity}% </h4>
-		</div>
-		<div id="windSpeed">
-			<h4> Wind Speed {weather.wind.speed} m/s </h4>
+	<div id="weatherWidget" >
+		
+		<div id="holder">
+			<div id="icon">
+				<img src={'https:' + weather.current.condition.icon} />	
+			</div>
+			<div id="sub-holder">
+				<div id="summaryWeather">
+					<h2> It's {weather.current.condition.text } </h2>
+					<h4> in {weather.location.name}, {weather.location.country} </h4>
+				</div>
+				<div id="temperature"> 
+					<h4> Temperature <span className="content">{ weather.current.temp_c }°C</span></h4>
+					<h6> (Feels like { weather.current.feelslike_c } °C)</h6>
+				</div>
+				<div id="humidity">
+					<h4> Humidity <span className="content">{weather.current.humidity}%</span> </h4>
+				</div>
+				<div id="windSpeed">
+					<h4> Wind Speed <span className="content">{weather.current.wind_mph} mph</span> </h4>
+				</div>
+			</div>
 		</div>	  
 	</div>
-	
 	: 
 	<div id="weather-loader">
         <i className="fa fa-circle-o-notch fa-spin fa-5x fa-fw"></i><br />
